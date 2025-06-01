8704d3831efd9dd4c1242600be8e459dde6a9406
bala-murugan-flutter <balamurugan@meditechnos.com>
2025-06-01 11:37:54 +0100

    main changedfs

--- constants/index.ts
+++ constants/index.ts
@@ -265,21 +265,21 @@ export const PROJECTS = [
     title: "Modern Next.js 14 Portfolio",
     description:
       'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
-    image: "/projects/project-1.png",
+    image: "/projects/project-1.jpg",
     link: "https://example.com",
   },
   {
     title: "Interactive Cards Portfolio",
     description:
       'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience. Ditching the conventional static layout, this portfolio leverages interactive cards to showcase my skills, projects, and personality in an engaging and dynamic manner.',
-    image: "/projects/project-2.png",
+    image: "/projects/project-2.jpg",
     link: "https://example.com",
   },
   {
     title: "Space Themed Website",
     description:
       'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world. Immerse yourself in an awe-inspiring digital experience that blends cutting-edge design with the mysteries of the universe.',
-    image: "/projects/project-3.png",
+    image: "/projects/project-3.jpg",
     link: "https://example.com",
   },
 ] as const;
